203945b92e30e79946e4a563a96b35fc0dc3146c
Laura Barreto <labarreto@vassar.edu>
2016-02-20 14:22:34 -0500

    working with firebase...setting up database

--- application.js
+++ application.js
@@ -1,3 +1,36 @@
 var myFirebaseApp = "motivateher";
 
-var recommendations = new Firebase("https://" + myFirebaseApp)
+// debugger
+
+
+var url = new Firebase("https://" + myFirebaseApp + ".firebaseio.com/");
+var goals = url.child("goals");
+var categories = url.child("categories");
+
+var submitGoal = function(){
+	var title = $("#goalTitle").val();
+	var link = $("#goalLink").val();
+	var frequency = $("#goalFrequency").val();
+	var total = $("#goalTotal").val();
+
+	goals.push({
+		"title": title,
+		"category": "health",
+		"progress": {
+			"frequency": frequency,
+			"total": total
+		}
+		
+	});
+
+};
+
+categories.set({
+	"health": {"subcategories": ["exercise", "food", "habits"]},
+	"education": {"subcategories": ["humanities", "technology", "science"]},
+	"skills": {"subcategories": ["crafts", "culinary", "music"]}
+}
+// , function (error) {
+// 	debugger
+// }
+);
